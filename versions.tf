38ac931c4c8d020772e582b039d9b582027e2e9b
Dalton Sena <daltonsena@users.noreply.github.com>
2025-02-28 20:32:36 -0300

    chore: ~> 4.0
    Signed-off-by: Dalton Sena <daltonsena@users.noreply.github.com>

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = ">= 2.19"
+      version = "~> 4.0"
     }
   }
   required_version = ">= 0.14"
